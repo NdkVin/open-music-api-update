a989a6557d3b1e199b3bcc23d1486846174c9dfd
ndkvin <andika.kavin@gmail.com>
2021-12-30 01:22:14 +0700

    add fk

--- src/validator/albums/schema.js
+++ src/validator/albums/schema.js
@@ -1,8 +1,16 @@
 const Joi = require('joi');
 
 const AlbumPayloadSchema = Joi.object({
-  name: Joi.string().required(),
-  year: Joi.number().required(),
+  name: Joi.string().required().messages({
+    'string.base': 'name harus berupa string',
+    'string.empty': 'name tidak boleh kosong',
+    'any.required': 'name harus diisi',
+  }),
+  year: Joi.number().required().messages({
+    'number.base': 'year harus berupa number',
+    'number.empty': 'year tidak boleh kosong',
+    'any.required': 'year harus diisi',
+  }),
 });
 
 module.exports = { AlbumPayloadSchema };
--- src/validator/songs/schema.js
+++ src/validator/songs/schema.js
@@ -1,12 +1,34 @@
 const Joi = require('joi');
 
 const SongPayloadSchema = Joi.object({
-  title: Joi.string().required(),
-  year: Joi.number().required(),
-  performer: Joi.string().required(),
-  genre: Joi.string().required(),
-  duration: Joi.number(),
-  albumId: Joi.string(),
+  title: Joi.string().required().messages({
+    'string.base': 'title harus berupa string',
+    'string.empty': 'title tidak boleh kosong',
+    'any.required': 'title harus diisi',
+  }),
+  year: Joi.number().required().messages({
+    'number.base': 'name harus berupa number',
+    'number.empty': 'name tidak boleh kosong',
+    'any.required': 'name harus diisi',
+  }),
+  performer: Joi.string().required().messages({
+    'string.base': 'performer harus berupa string',
+    'string.empty': 'performer tidak boleh kosong',
+    'any.required': 'performer harus diisi',
+  }),
+  genre: Joi.string().required().messages({
+    'string.base': 'genre harus berupa string',
+    'string.empty': 'genre tidak boleh kosong',
+    'any.required': 'genre harus diisi',
+  }),
+  duration: Joi.number().messages({
+    'number.base': 'duration harus berupa number',
+    'number.empty': 'duration tidak boleh kosong',
+  }),
+  albumId: Joi.string().messages({
+    'number.base': 'albumId harus berupa number',
+    'number.empty': 'albumId tidak boleh kosong',
+  }),
 });
 
 module.exports = { SongPayloadSchema };
