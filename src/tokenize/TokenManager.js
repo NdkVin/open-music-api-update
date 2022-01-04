12e0c6dda2d41e1c8ec9ab2702a36711465b3e59
ndkvin <andika.kavin@gmail.com>
2022-01-04 15:12:26 +0700

    edit refresh token

--- src/tokenize/TokenManager.js
+++ src/tokenize/TokenManager.js
@@ -3,11 +3,11 @@ const InvariantError = require('../exceptions/InvariantError');
 
 const TokenManager = {
   generateAccessToken: (payload) => Jwt.token.generate(payload, process.env.ACCESS_TOKEN_KEY),
-  generateRefreshToken: (payload) => Jwt.token.generate(payload, process.env.REFRESH_TOEKN_KEY),
+  generateRefreshToken: (payload) => Jwt.token.generate(payload, process.env.REFRESH_TOKEN_KEY),
   verifyRefreshToken: (refreshToken) => {
     try {
       const artifacts = Jwt.token.decode(refreshToken);
-      Jwt.token.verifySignature(artifacts, process.env.REFRESH_TOEKN_KEY);
+      Jwt.token.verifySignature(artifacts, process.env.REFRESH_TOKEN_KEY);
       const { payload } = artifacts.decoded;
       return payload;
     } catch (e) {
